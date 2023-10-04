e52e7ef74a8aed6b9d965e51ffd73e75c77fa7c5
Zachary Booth <zbooth.dev@gmail.com>
2023-10-04 09:52:36 -0500

    bufferline: Add plugin
    Add bufferline.

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        separator_style = "slant",
+        mode = "buffers",
+        -- stylua: ignore
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      },
+    },
+  },
+}
